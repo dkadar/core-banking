8cfabbbd42e3524afad5a2bcf6e10cfa39f3dae9
Tudorka <dylm93@gmail.com>
2018-10-11 00:04:17 +0200

    Remove unnecessary comments.

--- src/main/java/io/supercharge/homework/account/BankAccountHistoryRepository.java
+++ src/main/java/io/supercharge/homework/account/BankAccountHistoryRepository.java
@@ -11,13 +11,10 @@ import io.supercharge.homework.transaction.TransactionType;
 @Repository
 public interface BankAccountHistoryRepository extends JpaRepository<BankAccountHistory, Long>{
 
-//	@Query("SELECT h FROM BankAccountHistory h WHERE h.account.id = :bankAccountId ORDER BY h.transaction.date")
 	List<BankAccountHistory> findByAccountIdOrderByTransactionDate(long bankAccountId);
 
-//	@Query("SELECT h FROM BankAccountHistory h WHERE h.account.id = :bankAccountId AND h.transaction.date = :date")
 	List<BankAccountHistory> findByAccountIdAndTransactionDate(long bankAccountId, LocalDate date);
 
-//	@Query("SELECT h FROM BankAccountHistory h WHERE h.account.id = :bankAccountId AND h.transaction.type = :type")
 	List<BankAccountHistory> findByAccountIdAndTransactionType(long bankAccountId, TransactionType type);
 
 }
